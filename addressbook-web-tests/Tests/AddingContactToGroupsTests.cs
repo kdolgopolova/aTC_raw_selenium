6121d5aa93ddfcb1f760eeec8fea43e01c345902
Ksenia <kseniia.dolgopolova@gmail.com>
2022-02-10 02:24:08 +0300

    Task 17. Added handling no contacts or no groups

--- addressbook-web-tests/Tests/AddingContactToGroupsTests.cs
+++ addressbook-web-tests/Tests/AddingContactToGroupsTests.cs
@@ -1,21 +1,43 @@
-﻿using Newtonsoft.Json;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Xml.Serialization;
-using Excel = Microsoft.Office.Interop.Excel;
 namespace Addressbook_web_tests
 {
     [TestFixture]
     public class AddingContactToGroupsTests : AuthTestBase
     {
         [Test]
-        public void TestAddingContactToGroup() 
+        public void TestAddingContactToGroup()
         {
+            List<GroupData> groupList = GroupData.GetAll();
+            List<ContactData> contactList = ContactData.GetAll();
+            ContactData newContactData = new ContactData("AddedTo", $"AddingToGroup{R.Next(0,100)}");
+            GroupData newGroupData = new GroupData("Automatic Creation");
+
+            // Checks if groups & contacts exist at all
+            if (groupList.Count == 0)
+            {
+                app.Groups.Create(newGroupData);
+
+                if (contactList.Count == 0)
+                {
+                    app.Contacts.Create(newContactData);
+                }
+            }
+            if (contactList.Count == 0)
+            {
+                app.Contacts.Create(newContactData);
+            }
+
             GroupData group = GroupData.GetAll()[0];
             List<ContactData> oldData = group.GetContacts();
+
+            if (oldData.SequenceEqual(ContactData.GetAll()))
+            {
+                app.Contacts.Create(newContactData);
+            }
+
             ContactData contacts = ContactData.GetAll().Except(oldData).First();
 
             app.Contacts.AddContactToGroup(contacts, group);
--- addressbook-web-tests/Tests/RemovingContactFromGroupTests.cs
+++ addressbook-web-tests/Tests/RemovingContactFromGroupTests.cs
@@ -1,11 +1,6 @@
-﻿using Newtonsoft.Json;
-using NUnit.Framework;
-using System;
+﻿using NUnit.Framework;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Xml.Serialization;
-using Excel = Microsoft.Office.Interop.Excel;
 namespace Addressbook_web_tests
 {
     [TestFixture]
@@ -14,10 +9,46 @@ namespace Addressbook_web_tests
         [Test]
         public void RemovingContactFromGroup()
         {
-            int indexGroupToRemoveFrom = 1;
-            GroupData group = GroupData.GetAll()[indexGroupToRemoveFrom-1];
+            List<GroupData> groupList = GroupData.GetAll();
+            List<ContactData> contactList = ContactData.GetAll();
+            ContactData newContactData = new ContactData("AddedTo", $"AddingToGroup{R.Next(0, 100)}");
+            GroupData newGroupData = new GroupData("Automatic Creation");
+
+            // Checks if groups & contacts exist at all
+            if (groupList.Count == 0)
+            {
+                app.Groups.Create(newGroupData);
+
+                if (contactList.Count == 0)
+                {
+                    app.Contacts.Create(newContactData);
+                    GroupData tempGroup = GroupData.GetAll()[0];
+                    List<ContactData> tempOldData = tempGroup.GetContacts();
+                    ContactData tempContact = ContactData.GetAll().Except(tempOldData).First();
+                    app.Contacts.AddContactToGroup(tempContact, tempGroup);
+                }
+            }
+            else 
+            {
+                if (contactList.Count == 0)
+                {
+                    app.Contacts.Create(newContactData);
+                }
+
+                GroupData tempGroup = GroupData.GetAll()[0];
+                List<ContactData> tempOldData = tempGroup.GetContacts();
+
+                // Checks if no Contacts are added to the group
+                if (tempOldData.Count == 0)
+                {
+                    ContactData tempContact = ContactData.GetAll().Except(tempOldData).First();
+                    app.Contacts.AddContactToGroup(tempContact, tempGroup);
+                }
+            }
+
+            GroupData group = GroupData.GetAll()[0];
             List<ContactData> oldData = group.GetContacts();
-            ContactData contact = GroupData.GetAll()[indexGroupToRemoveFrom-1].GetContacts().First();
+            ContactData contact = GroupData.GetAll()[0].GetContacts().First();
 
             app.Contacts.RemoveContactFromGroup(contact, group);
 
@@ -25,6 +56,7 @@ namespace Addressbook_web_tests
             oldData.Remove(contact);
             oldData.Sort();
             newData.Sort();
+
             Assert.AreEqual(oldData, newData);
         }
     }
